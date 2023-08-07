24b4668a93b265163c183a4674828b621a48f615
Noel-96 <noelorion69@gmail.com>
2023-08-07 11:42:26 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PitchFormation
+
+A description of this package.
---
+++ Sources/PitchFormation/PitchFormation.swift
@@ -0,0 +1,6 @@
+public struct PitchFormation {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
